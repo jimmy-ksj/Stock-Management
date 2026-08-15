a97faee521c6533a7f5cf791b681d00f0c935d53
jimmy-ksj <karindasouvenir@gmail.com>
2026-08-16 00:29:00 +0200

    Delete Stock_Management_System directory

--- Stock_Management_System/frontend/src/components/Layout.jsx
+++
@@ -1,1 +0,0 @@
-import Navbar from "./Navbar";export default function Layout({children}){return <div className="app"><Navbar/><main className="main">{children}</main></div>}
--- Stock_Management_System/frontend/src/services/api.js
+++
@@ -1,4 +0,0 @@
-import axios from "axios";
-const api=axios.create({baseURL:"http://localhost:5000/api"});
-api.interceptors.request.use(c=>{const t=localStorage.getItem("token");if(t)c.headers.authorization=t;return c});
-export default api;
