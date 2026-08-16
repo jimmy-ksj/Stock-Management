bcff8d46695f543e42b2d1fa2541b34e01599b37
jimmy-ksj <karindasouvenir@gmail.com>
2026-08-16 13:09:43 +0200

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,14 +7,17 @@ const api = axios.create({
   },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
 
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
 
-  return config;
-});
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default api;
